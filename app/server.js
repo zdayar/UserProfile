3564bacc5d0833843b8fc4236a47d2ad4dcdb74a
Zeynep Dayar <zeynep.dayar@gmail.com>
2018-09-25 08:21:45 -0400

    more image file issues

--- app/server.js
+++ app/server.js
@@ -13,6 +13,8 @@ var express = require("express");
 // Tells node that we are creating an "express" server
 var app = express();
 
+app.use(express.static('Client/public'));
+
 // Sets an initial port. We"ll use this later in our listener
 var PORT = process.env.PORT || 8080;
 
